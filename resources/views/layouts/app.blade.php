e9f1acfeebc3d81e2d6c1a562f8304bdbae04aff
axeltux <axeltux@gmail.com>
2017-05-15 00:47:53 -0500

    Envio inicial

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Reddit Clone</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+</head>
+<body>
+	<div class="container">
+
+		<div class="row">
+			<div class="col-md-12">
+				<h1>Reddit Clone New Tittle</h1>
+			</div>
+		</div>
+
+		<hr>
+			@yield('content')
+	</div>	
+
+</body>
+</html>
